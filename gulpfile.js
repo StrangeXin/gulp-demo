c0cb6cc932dfbdc34ed792fbf891c2cef2b5fa7a
hexin <206034768@qq.com>
2021-01-25 18:30:31 +0800

    up

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp')
 var RevAll = require('gulp-rev-all')
+var rev = require('gulp-rev')
 var clean = require('gulp-clean')
 var RevClean = require('gulp-rev-dist-clean')
 
@@ -24,14 +25,29 @@ function defaultTask(cb) {
 gulp.task("default", function () {
   return gulp.src(["src/**"])
     .pipe(RevAll.revision({ dontRenameFile: [".html"] }))
-    .pipe(gulp.dest('dist'));
-    // .pipe(RevAll.manifestFile())
-    // .pipe(gulp.dest('dist'))
-    // .pipe(RevAll.versionFile())
-    // .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('dist'))
+    .pipe(RevAll.manifestFile())
+    .pipe(gulp.dest('dist'))
+  // .pipe(RevAll.versionFile())
+  // .pipe(gulp.dest('dist'));
 })
 
-gulp.task("clean", function () {
-  return gulp.src("dist/*")
-    .pipe(clean({force: true}))
+gulp.task("rev", function () {
+  return gulp.src(["src/**"])
+    .pipe(RevAll.revision({ dontRenameFile: [".html"] }))
+    .pipe(gulp.dest('dist'))
+    .pipe(RevAll.manifestFile())
+    .pipe(gulp.dest('dist'))
+  // .pipe(RevAll.versionFile())
+  // .pipe(gulp.dest('dist'));
+})
+
+// gulp.task("clean", function () {
+//   return gulp.src("dist/*")
+//     .pipe(clean({force: true}))
+// })
+
+gulp.task("cleana", function () {
+  return gulp.src(['dist/js/*'])
+    .pipe(RevClean('dist/rev-manifest.json'))
 })
