6b8f576e00f91fcfe6a3a76e93a069289ceccb1d
hexin <206034768@qq.com>
2021-01-26 16:45:45 +0800

    up

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 const { src, dest, series } = require('gulp')
 const revAll = require('gulp-rev-all') //给静态资源添加hash值，并更新文件依赖
 const del = require('del')  //删除文件夹
-// const useref = require('gulp-useref'); //按规则合并js、css
 const browserSync = require('browser-sync').create() //多设备实时浏览器同步更新服务
 const { createProxyMiddleware } = require('http-proxy-middleware') //本地开发代理跨域请求插件
 const proxy = createProxyMiddleware(['/api'],{
@@ -11,9 +10,6 @@ const proxy = createProxyMiddleware(['/api'],{
     '/api': ''
   }
 })
-const uglify = require('gulp-uglify') //压缩js
-const htmlmin = require('gulp-htmlmin') //压缩html
-
 
 //清空目录
 function del_dist() {
@@ -25,15 +21,6 @@ function build() {
   return src(["src/**"])
     .pipe(revAll.revision({ dontRenameFile: [".html"] }))
     .pipe(dest('dist'));
-  // .pipe(revAll.manifestFile())
-  // .pipe(dest('dist'));
-}
-
-//合并html里的js、css
-function html_useref() {
-  return src(["dist/*.html"])
-    .pipe(useref())
-    .pipe(dest('dist'));
 }
 
 //开发静态服务器
@@ -43,23 +30,9 @@ function browser_sync() {
     port: 9000,
     server: {
       baseDir: "./src",
-      middleware: [proxy]
+      middleware: [proxy]  //配置代理
     }
   })
-}
-
-//js压缩
-function compress() {
-  return src(["src/*.js"])
-    .pipe(uglify())
-    .pipe(dest('dist'));
-}
-
-//html压缩
-function minify() {
-  return src(["src/*.html"])
-    .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(dest('dist'));
 }
 
 exports.browser = series(browser_sync);
